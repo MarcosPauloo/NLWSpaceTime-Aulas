9de9a5e8260b882e01e3e2205697157d4089fa06
mpos <olisilva.marcospaulo@gmail.com>
2023-05-17 22:46:06 -0300

    iniciando producao da aplicacao web e mobile

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import {
+  Bai_Jamjuree as BaiJamjuree,
+  Roboto_Flex as Roboto,
+} from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamJure = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Setup',
+  description:
+    'Uma cápsula do tempo construida com React, Next.js, TaildwindCSS e TypeScript.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamJure.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
